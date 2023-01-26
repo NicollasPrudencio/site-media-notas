6959b164c5b47d7c770f0a02047efe47af1fefc5
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-01-25 23:42:06 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji festejando">';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado">';
 const atividades = [];
 const notas = [];
-
+const spanAprovado = '<span class= "resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class= "resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima: "));
 
 let linhas = '';
 
@@ -19,16 +21,20 @@ function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade ${inputNomeAtividade.value} já foi inserida.`)
+    } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
 
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
-    linha += '</tr>';
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
 
-    linhas += linha;
+        linhas += linha;
+    }
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
@@ -50,8 +56,8 @@ function calculaMediaFinal() {
 }
 
 function atualizaMediaFinal() {
-const mediaFinal = calculaMediaFinal();
+    const mediaFinal = calculaMediaFinal();
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >=7 ? 'Aprovado' : 'Reprovado';
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
